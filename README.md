9f19c2d080664bff4ee7e0c75fa53ecfaaade88c
misakanetwork2018 <moqiaoduo@gmail.com>
2018-10-30 00:20:38 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 该脚本会把ssr安装到/root下，所以请在root用户下执行
 
 一键命令：wget --no-check-certificate -O ./install.sh https://raw.githubusercontent.com/misakanetwork2018/one_click_install_ssr_manyuser/master/install.sh && sh install.sh
+
+本脚本可接收以下参数，以便全自动部署：
+-s|--supervisor  安装后台守护程序
+-r|--run 安装完毕后立刻运行守护程序
+-P|--port= 修改usermysql.json中的port
+-p|--password= 修改usermysql.json中的password
+-h|--host= 修改usermysql.json中的host
+-u|--username 修改usermysql.json中的username
+-n|--node 修改usermysql.json中的node
+-d|--db 修改usermysql.json中的db
--- install.sh
+++ install.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+GETOPT_ARGS=`getopt -o sp:h:u:P:n:d:r: -l supervisor,password:,host:,username:,port:,node:,db:,run -- "$@"`
+eval set -- "$GETOPT_ARGS"
+OLD_IFS="$IFS"
+IFS=" "
+arguments=($*)
+IFS="$OLD_IFS"
+supervisor=false
+password=""
+username=""
+port=""
+host=""
+node=""
+db=""
+run=false
+
+#获取参数
+while [ -n "$1" ]
+do
+	case "$1" in
+		-s|--supervisor) supervisor=true;shift 1;;
+		-r|--run) run=true;shift 1;;
+                -P|--port) port=$2;shift 2;;
+                -p|--password) password=$2;shift 2;;
+                -h|--host) host=$2;shift 2;;
+                -u|--username) username=$2;shift 2;;
+                -n|--node) node=$2;shift 2;;
+                -d|--db) db=$2;shift 2;;
+                --) break ;;
+                *) break ;;
+        esac
+done
+
 #获得系统类型
 Get_Dist_Name()
 {
@@ -83,5 +115,62 @@ fi
 cd shadowsocksr
 ./setup_cymysql.sh
 ./initcfg.sh
+random_p=`date +%s%N | md5sum |cut -c 1-16`
+sed -i 's/"additional_ports" : {}/"additional_ports" : {"9000":{"password":"'$random_p'"}}' user-config.json
+
+if $supervisor; then
+   echo "Installing supervisor..."
+cd /usr/local/src
+wget https://pypi.python.org/packages/7b/17/88adf8cb25f80e2bc0d18e094fcd7ab300632ea00b601cbbbb84c2419eae/supervisor-3.3.2.tar.gz
+tar -zxvf supervisor-3.3.2.tar.gz
+cd supervisor-3.3.2
+python setup.py install
+   echo_supervisord_conf > /etc/supervisord.conf
+file=<<EOF
+[include]
+files=/etc/supervisor/*.conf #若你本地无/etc/supervisor目录，请自建
+EOF
+echo $file>> /etc/supervisord.conf
+mkdir -p /etc/supervisor
+file=<<EOF
+; 设置进程的名称，使用 supervisorctl 来管理进程时需要使用该进程名
+[program:shadowsocksr]
+command=python server.py
+;numprocs=1 ; 默认为1 
+;process_name=%(program_name)s ; 默认为 %(program_name)s，即 [program:x] 中的 x 
+directory=/root/shadowsocksr ; 执行 command 之前，先切换到工作目录
+user=root ; 使用 oxygen 用户来启动该进程
+; 程序崩溃时自动重启，重启次数是有限制的，默认为3次 autorestart=true 
+redirect_stderr=true
+; 重定向输出的日志
+stdout_logfile = /var/log/supervisord/tornado_server.log
+loglevel=info
+EOF
+echo $file>/etc/supervisor/ssr.conf
+fi
+
+if [[ -n "$username" ]]; then
+sed -i 's/"user": "ss"/"user": "'${username}'"/' usermysql.json
+fi
+if [[ -n "$password" ]]; then
+sed -i 's/"password": "pass"/"password": "'${password}'"/' usermysql.json
+fi
+if [[ -n "$port" ]]; then
+sed -i 's/"port": 3306/"port": '${port}'/' usermysql.json
+fi
+if [[ -n "$host" ]]; then
+sed -i 's/"host": "127.0.0.1"/"host": "'${host}'/"' usermysql.json
+fi
+if [[ -n "$db" ]]; then
+sed -i 's/"db": "sspanel"/"db": "'${host}'/"' usermysql.json
+fi
+if [[ -n "$node" ]]; then
+sed -i 's/"node_id": 0/"node_id": '${node}'/' usermysql.json
+fi
+
+if $run; then
+supervisord -c /etc/supervisord.conf
+supervisorctl reload
+fi
 
 echo "Install completely."
